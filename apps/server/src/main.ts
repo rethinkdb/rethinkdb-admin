fd639f68c5a6579b12e00f200d94c513ee89f91e
atassis <atassikay38@gmail.com>
2022-05-11 00:10:57 +0800

    feat: added compression for production env

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,17 +1,24 @@
 declare const module: any;
 import { NestFactory } from '@nestjs/core';
+import compression from 'compression';
+
 import { AppModule } from './app.module';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  if (process.env.NODE_ENV === 'development') {
+  if (isDevelopment) {
     const devModule = await import('./dev');
     devModule.initDev(app);
   }
+  if (!isDevelopment) {
+    app.use(compression());
+  }
   await app.listen(3000);
 
-  if (process.env.NODE_ENV === 'development') {
+  if (isDevelopment) {
     if (module.hot) {
       module.hot.accept();
       module.hot.dispose(() => app.close());
